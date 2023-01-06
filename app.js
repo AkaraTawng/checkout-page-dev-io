8176314bb07cc3d567ac7b30a4a0f922ea916e13
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-01-07 06:42:51 +0700

    implement decrement function

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const decrementBtn = document.querySelector("#decrement");
 const incrementBtn = document.querySelector("#increment");
 const quantity = document.querySelector("#quantity");
 
-let counter = 0;
+let counter = 5;
 quantity.innerText=counter;
 
 const decrement = () => {
-    
+    if(counter > 0){
+          counter--;
+    quantity.innerText=counter;
+    }
+    return counter;
 }
