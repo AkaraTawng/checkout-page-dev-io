69dcb3a1d27c6da6823f8997874dc1ff29fb1316
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-01-07 06:35:15 +0700

    set quantity inner text to counter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const incrementBtn = document.querySelector("#increment");
 const quantity = document.querySelector("#quantity");
 
 let counter = 0;
+quantity.innerText=counter;
 
 const decrement = () => {
     
