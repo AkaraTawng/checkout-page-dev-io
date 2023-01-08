1760d8f3a08e6be57e8bcd1d3d03a3270528585d
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-01-09 06:36:01 +0700

    add products.js file. add products array.

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        id: 0, 
+        name: 'Vintage Backpack', 
+        oldPrice: 94.99, 
+        discountPrice: 54.99, 
+        imgSrc: './img/bag.png', 
+        quantity: 0
+    }, 
+    {
+        id: 0, 
+        name: 'Levi Shoes', 
+        oldPrice: 124.99, 
+        discountPrice: 74.99, 
+        imgSrc: './img/shoes.png', 
+        quantity: 0
+    }
+]
