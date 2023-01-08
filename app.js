80140f8151dfa6dd82bc060dcd25866c8894cf13
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-01-09 06:50:35 +0700

    declare basket variable. make render basket function. map over products array.

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const decrementBtn = document.querySelector("#decrement");
 const incrementBtn = document.querySelector("#increment");
 const quantity = document.querySelector("#quantity");
 
+const basket = document.querySelector("#basket");
+
+const renderBasket = () => {
+    products.forEach(product => {
+        basket.innerHTML += `
+        <div class="basket-item-outer-container">
+        <img src="./img/bag.png" alt="backpack">
+            <div class="basket-item-inner-container">
+                <div class="basket-item-info">
+                    <h2>Vintage Backpack</h2>
+                    <p>$54.99 <span>$94.99</span></p>
+                </div>
+                <div id="basket-item-quantity">
+                    <button id="decrement">-</button>
+                    <h2 id="quantity"></h2>
+                    <button id="increment">+</button>
+                </div>
+            </div>
+    </div>
+        `
+    })
+}
+
+renderBasket();
+
+
 let counter = 0;
 quantity.innerText=counter;
 
@@ -19,4 +45,4 @@ const increment = () => {
 }
 
 /* add delete item function. if item is 0, remove from basket. */
-/* add products js file as database for products */
+/* add products js file as database for products */ 
